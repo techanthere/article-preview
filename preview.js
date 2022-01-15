d367e2085a9fb12367d3e7304b04b3edbd314bc6
techant <techanthere@gmail.com>
2022-01-15 10:42:20 +0500

    use of better aria annotations

--- preview.js
+++ preview.js
@@ -3,17 +3,19 @@
 */
 
 function shareFunction(){
-  const popup = document.querySelector(".main__footer--popup");
+  const popup = document.querySelector(".main___footer-popup");
   const share = document.getElementById("click");
   const icon = document.querySelector(".share_icon");
   if (popup.style.display === "flex") {
     popup.style.display = "none";
     share.style.backgroundColor = "var(--light-grayish-blue)";
+    share.setAttribute("aria-expanded", false);
     icon.style.mixBlendMode = "normal";
   }
   else{
     popup.style.display = "flex";
     share.style.backgroundColor = "var(--some-gray-shade)";
+    share.setAttribute("aria-expanded", true);
     icon.style.mixBlendMode = "color-dodge"
   }
 }
